559ca047487477927d8b101de3cd8b2b384b88fd
playper3 <saper6569@gmail.com>
2021-11-28 12:30:02 -0800

    Light engine setup methods

--- src/code/environment/Lighting.java
+++ src/code/environment/Lighting.java
@@ -1,7 +1,10 @@
 package code.environment;
 
 import com.jme3.light.AmbientLight;
+import com.jme3.light.SpotLight;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.renderer.Camera;
 
 public class Lighting {
 
@@ -11,5 +14,14 @@ public class Lighting {
         return light;
     }
 
-    public static 
+    public static SpotLight setupLight(Camera cam) {
+        SpotLight spot = new SpotLight();
+        spot.setSpotRange(100f);                           // distance
+        spot.setSpotInnerAngle(15f * FastMath.DEG_TO_RAD); // inner light cone (central beam)
+        spot.setSpotOuterAngle(35f * FastMath.DEG_TO_RAD); // outer light cone (edge of the light)
+        spot.setColor(ColorRGBA.White.mult(1.3f));         // light color
+        spot.setPosition(cam.getLocation());               // shine from camera loc
+        spot.setDirection(cam.getDirection());             // shine forward from camera loc
+        return spot;
+    }
 }
