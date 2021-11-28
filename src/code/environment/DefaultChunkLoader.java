d1ef7a106d3289c54ed3cfb5dd16782ba88b27fc
playper3 <saper6569@gmail.com>
2021-11-28 12:34:55 -0800

    default chunk loading system

---
+++ src/code/environment/DefaultChunkLoader.java
@@ -0,0 +1,40 @@
+package code.environment;
+
+import code.screens.MainScreenInit;
+import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.bullet.util.CollisionShapeFactory;
+import com.jme3.scene.Spatial;
+
+public class DefaultChunkLoader extends AbstractAppState {
+
+    private SimpleApplication app;
+    private Spatial map;
+    private RigidBodyControl mapSpace;
+    private BulletAppState bulletAppState;
+    private MainScreenInit mainScreen = new MainScreenInit();
+
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+        super.initialize(stateManager, app);
+        this.app = (SimpleApplication) app;
+
+        map = this.app.getAssetManager().loadModel("Models/hallway.gltf");
+        map.setLocalScale(2.5f);
+        this.app.getRootNode().attachChild(map);
+
+        CollisionShape sceneShape = CollisionShapeFactory.createMeshShape(map);
+        mapSpace = new RigidBodyControl(sceneShape, 0);
+        map.addControl(mapSpace);
+
+        bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
+        bulletAppState.getPhysicsSpace().add(mapSpace);
+        bulletAppState.getPhysicsSpace().add(mainScreen.getPlayer());
+    }
+}
