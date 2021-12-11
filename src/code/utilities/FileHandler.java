4d08e4d286cb3427acd1ff5c00efe7e27ed9c23e
playper3 <saper6569@gmail.com>
2021-12-11 13:27:24 -0800

    added file reading method

--- src/code/utilities/FileHandler.java
+++ src/code/utilities/FileHandler.java
@@ -1,5 +1,19 @@
 package code.utilities;
 
+import code.environment.ChunkLoader;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class FileHandler {
 
+    public void read(String fileName) throws IOException {
+        File file = new File(fileName);
+        Scanner sc = new Scanner(file);
+        ChunkLoader chunkLoader = new ChunkLoader();
+        while (sc.hasNextLine()) {
+            chunkLoader.loadChunk(sc.nextLine());
+        }
+    }
 }
